d38386a2f79b8c6c33ce05b114ae804524d9cf04
ClementNg <ng2b30@hotmail.com>
2016-01-18 22:53:05 +0800

    update alarm

---
+++ app/src/main/java/com/example/yuen/e_carei/queueshow.java
@@ -0,0 +1,196 @@
+package com.example.yuen.e_carei;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.example.yuen.e_carei_app.AppController;
+import com.example.yuen.e_carei_login.SQLiteHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+public class queueshow extends AppCompatActivity {
+
+    private static final String queue_show_url = "http://192.168.43.216/test/queue_show.php";
+    private DrawerLayout drawerLayout;
+    int navItemId;
+    private Toolbar toolbar;
+    private static final String NAV_ITEM_ID = "nav_index";
+
+    private SQLiteHandler db;
+    private TextView uid_show , name ,queue_show;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.queue_show);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("E-care");
+        setSupportActionBar(toolbar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView view = (NavigationView) findViewById(R.id.navigation_view);
+        view.getMenu().getItem(1).setChecked(true);
+        view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                Toast.makeText(queueshow.this, menuItem.getItemId() + " pressed", Toast.LENGTH_LONG).show();
+                Log.d(R.id.nav_1 + "", menuItem.getItemId() + " ");
+                switch (menuItem.getItemId()) {
+
+
+                    case R.id.nav_1:
+                        Intent intent = new Intent();
+                        intent.setClass(queueshow.this, Case_history_review.class);
+                        //intent .putExtra("name", "Hello B Activity");
+                        startActivity(intent);
+                        break;
+                    case R.id.nav_2:
+
+                        break;
+                    case R.id.nav_3:
+                        break;
+                    case R.id.nav_4:
+                        break;
+                    case R.id.nav_5:
+                        break;
+                    case R.id.nav_6:
+                        break;
+
+                }
+                menuItem.setChecked(true);
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle( this, drawerLayout, toolbar, R.string.drawer_open , R.string.drawer_close){
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super .onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super .onDrawerOpened(drawerView);
+            }
+        };
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        uid_show = (TextView)findViewById(R.id.uid_show);
+        name = (TextView)findViewById(R.id.name_show);
+        queue_show= (TextView)findViewById(R.id.queue_show);
+        db = new SQLiteHandler(getApplicationContext());
+
+        HashMap<String, String> dbuser = db.getUserDetails();
+
+        //uid.setText(dbuser.get("uid"));
+        //name.setText(dbuser.get("name"));
+
+        //check to see the slot is full or not.1. add data to db and calculate the no of data in the db 2. If >5 , delete it
+        JsonArrayRequest movieReq = new JsonArrayRequest(queue_show_url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("Response", response.toString());
+                        //Log.d("id", "hi") ;
+                        // Parsing json
+                        // reset the list
+                        String uid="P001";
+                        queue_show.setText("0");
+                        uid_show.setText(uid);
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                //Log.d("id","hi1") ;
+                                //Log.d("i","i:"+i);
+                                //get id
+                                //Log.d("length", "length:" + response.length());
+                                JSONObject objuid = response.getJSONObject(i);
+                                //get id
+                                if(uid.equals(objuid.getString("uid")))
+                                uid_show.setText(objuid.getString("uid"));
+
+                                JSONObject objqueue= response.getJSONObject(++i);
+                                //get id
+                                if(uid.equals(objuid.getString("uid"))) {
+                                    queue_show.setText(objqueue.getString("queue"));
+                                }
+                                else
+                                    queue_show.setText("0");
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("Error", "Error: " + error.getMessage());
+
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(movieReq);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_case_history_review, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void navigateTo(MenuItem menuItem){
+
+        navItemId = menuItem.getItemId();
+        menuItem.setChecked(true);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(NAV_ITEM_ID, navItemId);
+    }
+}
---
+++ app/src/main/java/za/co/neilson/alarm/AlarmActivity.java
@@ -0,0 +1,233 @@
+/* Copyright 2014 Sheldon Neilson www.neilson.co.za
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+package za.co.neilson.alarm;
+
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.HapticFeedbackConstants;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.yuen.e_carei.Case_history_review;
+import com.example.yuen.e_carei.R;
+
+import java.util.List;
+
+import za.co.neilson.alarm.database.Database;
+import za.co.neilson.alarm.preferences.AlarmPreferencesActivity;
+
+public class AlarmActivity extends BaseActivity {
+
+	ImageButton newButton;
+	ListView mathAlarmListView;
+	AlarmListAdapter alarmListAdapter;
+	private DrawerLayout drawerLayout;
+	int navItemId;
+	private Toolbar toolbar;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.alarm_activity);
+
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		toolbar.setTitle("E-care");
+		setSupportActionBar(toolbar);
+
+		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		NavigationView view = (NavigationView) findViewById(R.id.navigation_view);
+		view.getMenu().getItem(1).setChecked(true);
+		view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+			@Override
+			public boolean onNavigationItemSelected(MenuItem menuItem) {
+				Toast.makeText(AlarmActivity.this, menuItem.getItemId() + " pressed", Toast.LENGTH_LONG).show();
+				Log.d(R.id.nav_1 + "", menuItem.getItemId() + " ");
+				switch (menuItem.getItemId()) {
+
+
+					case R.id.nav_1:
+						Intent intent = new Intent();
+						intent.setClass(AlarmActivity.this, Case_history_review.class);
+						//intent .putExtra("name", "Hello B Activity");
+						startActivity(intent);
+						break;
+					case R.id.nav_2:
+
+						break;
+					case R.id.nav_3:
+						break;
+					case R.id.nav_4:
+						break;
+					case R.id.nav_5:
+						break;
+					case R.id.nav_6:
+						break;
+
+				}
+				menuItem.setChecked(true);
+				drawerLayout.closeDrawers();
+				return true;
+			}
+		});
+
+		ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle( this, drawerLayout, toolbar, R.string.drawer_open , R.string.drawer_close){
+			@Override
+			public void onDrawerClosed(View drawerView) {
+				super .onDrawerClosed(drawerView);
+			}
+
+			@Override
+			public void onDrawerOpened(View drawerView) {
+				super .onDrawerOpened(drawerView);
+			}
+		};
+
+		drawerLayout.setDrawerListener(actionBarDrawerToggle);
+		actionBarDrawerToggle.syncState();
+
+		mathAlarmListView = (ListView) findViewById(android.R.id.list);
+		mathAlarmListView.setLongClickable(true);
+		mathAlarmListView.setOnItemLongClickListener(new OnItemLongClickListener() {
+			@Override
+			public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
+				view.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
+				final Alarm alarm = (Alarm) alarmListAdapter.getItem(position);
+				Builder dialog = new AlertDialog.Builder(AlarmActivity.this);
+				dialog.setTitle("Delete");
+				dialog.setMessage("Delete this alarm?");
+				dialog.setPositiveButton("Ok", new OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+
+						Database.init(AlarmActivity.this);
+						Database.deleteEntry(alarm);
+						AlarmActivity.this.callMathAlarmScheduleService();
+
+						updateAlarmList();
+					}
+				});
+				dialog.setNegativeButton("Cancel", new OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.dismiss();
+					}
+				});
+
+				dialog.show();
+
+				return true;
+			}
+		});
+
+		callMathAlarmScheduleService();
+
+		alarmListAdapter = new AlarmListAdapter(this);
+		this.mathAlarmListView.setAdapter(alarmListAdapter);
+		mathAlarmListView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View v, int position, long id) {
+				v.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+				Alarm alarm = (Alarm) alarmListAdapter.getItem(position);
+				Intent intent = new Intent(AlarmActivity.this, AlarmPreferencesActivity.class);
+				intent.putExtra("alarm", alarm);
+				startActivity(intent);
+			}
+
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		boolean result = super.onCreateOptionsMenu(menu);		
+		menu.findItem(R.id.menu_item_save).setVisible(false);
+		menu.findItem(R.id.menu_item_delete).setVisible(false);
+	    return result;
+	}
+		
+	@Override
+	protected void onPause() {
+		// setListAdapter(null);
+		Database.deactivate();
+		super.onPause();
+	}
+
+	private void navigateTo(MenuItem menuItem){
+
+		navItemId = menuItem.getItemId();
+		menuItem.setChecked(true);
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		//outState.putInt(NAV_ITEM_ID, navItemId);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		updateAlarmList();
+	}
+	
+	public void updateAlarmList(){
+		Database.init(AlarmActivity.this);
+		final List<Alarm> alarms = Database.getAll();
+		alarmListAdapter.setMathAlarms(alarms);
+		
+		runOnUiThread(new Runnable() {
+			public void run() {
+				// reload content			
+				AlarmActivity.this.alarmListAdapter.notifyDataSetChanged();				
+				if(alarms.size() > 0){
+					findViewById(android.R.id.empty).setVisibility(View.INVISIBLE);
+				}else{
+					findViewById(android.R.id.empty).setVisibility(View.VISIBLE);
+				}
+			}
+		});
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (v.getId() == R.id.checkBox_alarm_active) {
+			CheckBox checkBox = (CheckBox) v;
+			Alarm alarm = (Alarm) alarmListAdapter.getItem((Integer) checkBox.getTag());
+			alarm.setAlarmActive(checkBox.isChecked());
+			Database.update(alarm);
+			AlarmActivity.this.callMathAlarmScheduleService();
+			if (checkBox.isChecked()) {
+				Toast.makeText(AlarmActivity.this, alarm.getTimeUntilNextAlarmMessage(), Toast.LENGTH_LONG).show();
+			}
+		}
+
+	}
+
+}
